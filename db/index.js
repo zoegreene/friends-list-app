79a7ab94558e7a1b3686e6cfb58b2f9bd96cb9f4
Peet Klecha <p.a.klecha@gmail.com>
2020-08-23 14:11:05 -0400

    peets-comments

--- db/index.js
+++ db/index.js
@@ -1,22 +1,25 @@
-const Sequelize = require('sequelize');
-const { STRING, INTEGER } = Sequelize;
-const databaseUrl = process.env.DATABASE_URL || 'postgres://localhost/friendslist'
+const Sequelize = require("sequelize")
+const { STRING, INTEGER } = Sequelize
+const databaseUrl =
+	process.env.DATABASE_URL || "postgres://localhost/friendslist"
 
 const db = new Sequelize(databaseUrl, {
-  logging: false
-});
+	logging: false,
+})
 
-const Friend = db.define('friend', {
-  name: {
-    type: STRING,
-    allowNull: false
-  },
-  rating: {
-    type: INTEGER,
-    defaultValue: 5
-  }
-});
+const Friend = db.define("friend", {
+	name: {
+		type: STRING,
+		allowNull: false,
+		//[PK] unique?
+	},
+	rating: {
+		type: INTEGER,
+		defaultValue: 5,
+	},
+})
 
 module.exports = {
-  db, Friend
-};
+	db,
+	Friend,
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const ul = document.querySelector('ul');
+//[PK] I love this whole file! Super clean, modular, functionality is there. Great work!
 
-const displayFriends = async() => {
-  const result = await fetch('/api/friends');
-  const data = await result.json();
+const ul = document.querySelector("ul")
 
-  const html = data.map( friend => {
-    return `
+const displayFriends = async () => {
+	const result = await fetch("/api/friends")
+	const data = await result.json()
+
+	const html = data
+		.map(friend => {
+			return `
       <li id='${friend.name}-entry'>${friend.name}</li>
       <div id='${friend.id}'>
         <span>${friend.rating}</span>
@@ -14,69 +17,69 @@ const displayFriends = async() => {
         <button>x</button>
         <p></p>
       </div>
-    `;
-  }).join('');
-  ul.innerHTML = html;
+    `
+		})
+		.join("")
+	ul.innerHTML = html
 }
 
 const handleFriendOptions = () => {
-  ul.addEventListener('click', async function(event) {
-    try{
-      const target = event.target;
-      if (target.tagName === 'BUTTON') {
-        if (target.innerHTML === '+') {
-          changeRating(target, 1);
-        } else if (target.innerHTML === '-') {
-          changeRating(target, -1);
-        } else if (target.innerHTML === 'x') {
-          await fetch(`/api/friends/${event.target.parentNode.id}`, {
-            method: 'DELETE'
-          })
-          displayFriends();
-        }
-      }
-    }
-    catch(err) {
-      console.error(err)
-    }
-  });
+	ul.addEventListener("click", async function (event) {
+		try {
+			const target = event.target
+			if (target.tagName === "BUTTON") {
+				if (target.innerHTML === "+") {
+					changeRating(target, 1)
+				} else if (target.innerHTML === "-") {
+					changeRating(target, -1)
+				} else if (target.innerHTML === "x") {
+					await fetch(`/api/friends/${event.target.parentNode.id}`, {
+						method: "DELETE",
+					})
+					displayFriends()
+				}
+			}
+		} catch (err) {
+			console.error(err)
+		}
+	})
 }
 
 const addFriend = async () => {
-  const createButton = document.getElementById('create-friend');
+	const createButton = document.getElementById("create-friend")
 
-  createButton.addEventListener('click', async function() {
-    let newFriend = document.getElementById('new-friend').value;
-    await fetch(`/api/friends`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: newFriend
-      })
-    });
-    document.getElementById('new-friend').value = ""; // clear input box
-    displayFriends();
-  });
+	createButton.addEventListener("click", async function () {
+		let newFriend = document.getElementById("new-friend").value
+		await fetch(`/api/friends`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				name: newFriend,
+			}),
+		})
+		document.getElementById("new-friend").value = "" // clear input box
+		displayFriends()
+	})
 }
 
 const changeRating = async (target, direction) => {
-  let rating = parseInt(target.id.slice(0, -1)) + direction;
+	let rating = parseInt(target.id.slice(0, -1)) + direction
 
-  await fetch(`/api/friends/${target.parentNode.id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-      rating: rating
-    }),
-    headers: new Headers({
-      'Content-Type': 'application/json'
-    })
-  });
+	await fetch(`/api/friends/${target.parentNode.id}`, {
+		method: "PUT",
+		body: JSON.stringify({
+			rating: rating,
+		}),
+		headers: new Headers({
+			"Content-Type": "application/json",
+		}),
+	})
 
-  displayFriends();
+	displayFriends()
 }
 
-displayFriends();
-handleFriendOptions();
-addFriend();
+displayFriends()
+handleFriendOptions()
+addFriend()
